f9e655934a73305687509a064ee6dd6644f4dd3c
mori1704 <1704mori@gmail.com>
2023-12-01 16:43:58 -0300

    Add Docker container management functions

--- backend/docker/docker.go
+++ backend/docker/docker.go
@@ -2,13 +2,19 @@ package docker
 
 import (
 	"context"
+	"errors"
+	"fmt"
 
 	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/container"
 	"github.com/docker/docker/client"
 )
 
 type DockerAPI interface {
-	ContainerList(context.Context, types.ContainerListOptions) ([]types.Container, error)
+	ContainerList(ctx context.Context, options types.ContainerListOptions) ([]types.Container, error)
+	ContainerStart(ctx context.Context, containerID string, options types.ContainerStartOptions) error
+	ContainerStop(ctx context.Context, containerID string, options container.StopOptions) error
+	ContainerRestart(ctx context.Context, containerID string, options container.StopOptions) error
 }
 
 type Client struct {
@@ -29,10 +35,50 @@ func NewDockerClient() (*Client, error) {
 }
 
 func (d *Client) ListContainers() ([]types.Container, error) {
-	containers, err := d.cli.ContainerList(context.Background(), types.ContainerListOptions{})
+	containers, err := d.cli.ContainerList(context.Background(), types.ContainerListOptions{All: true})
 	if err != nil {
 		return nil, err
 	}
 
 	return containers, nil
+}
+
+func (d *Client) FindContainer(id string) (types.Container, error) {
+	var container types.Container
+
+	containers, err := d.ListContainers()
+	if err != nil {
+		return container, err
+	}
+
+	for _, c := range containers {
+		if c.ID == id {
+			container = c
+			break
+		}
+	}
+
+	if container.ID == "" {
+		return container, errors.New(fmt.Sprintf("unable to find container %s", id))
+	}
+
+	return container, nil
+}
+
+func (d *Client) StopContainer(id string) (bool, error) {
+	err := d.cli.ContainerStop(context.Background(), id, container.StopOptions{})
+	if err != nil {
+		return false, err
+	}
+
+	return true, nil
+}
+
+func (d *Client) RestartContainer(id string) (bool, error) {
+	err := d.cli.ContainerRestart(context.Background(), id, container.StopOptions{})
+	if err != nil {
+		return false, err
+	}
+
+	return true, nil
 }
