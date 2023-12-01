8ac2919732ec92f02fed85d3a51d6b3c85f9e10a
mori1704 <1704mori@gmail.com>
2023-12-01 16:43:46 -0300

    Add API endpoint for container actions

--- backend/api/routes.go
+++ backend/api/routes.go
@@ -2,6 +2,7 @@ package api
 
 import (
 	"encoding/json"
+	"fmt"
 	"time"
 
 	"github.com/1704mori/dokka/docker"
@@ -37,7 +38,7 @@ func APIRoutes(router *gin.Engine, dockerClient *docker.Client) {
 						sse <- "error: " + err.Error()
 					}
 
-					sse <- "data: " + string(marshal)
+					sse <- "data: " + string(marshal) + "\n\n"
 					time.Sleep(5 * time.Second)
 				}
 			}()
@@ -46,6 +47,84 @@ func APIRoutes(router *gin.Engine, dockerClient *docker.Client) {
 				msg := <-sse
 				ctx.SSEvent("message", msg)
 				ctx.Writer.Flush()
+			}
+		})
+		v1.POST("/container/:action/:id", func(ctx *gin.Context) {
+			action := ctx.Param("action")
+			id := ctx.Param("id")
+
+			if id == "" {
+				ctx.JSON(400, gin.H{
+					"result":  "error",
+					"message": "missing container id parameter",
+				})
+				return
+			}
+
+			if action == "" {
+				ctx.JSON(400, gin.H{
+					"result":  "error",
+					"message": "missing action parameter",
+				})
+				return
+			}
+
+			container, err := dockerClient.FindContainer(id)
+			if err != nil {
+				ctx.JSON(400, gin.H{
+					"result":  "error",
+					"message": fmt.Sprintf("container with id %s not found", id)})
+				return
+			}
+
+			if container.ID == "" {
+				ctx.JSON(400, gin.H{
+					"result":  "error",
+					"message": fmt.Sprintf("container with id %s not found", id)})
+				return
+			}
+
+			switch action {
+			case "stop":
+				_, err = dockerClient.StopContainer(id)
+				if err != nil {
+					ctx.JSON(500, gin.H{
+						"result":  "error",
+						"message": fmt.Sprintf("failed to stop container: %v", err),
+					})
+					return
+				}
+				ctx.JSON(200, gin.H{
+					"result":  "ok",
+					"message": fmt.Sprintf("container %s stopped successfully", id),
+				})
+			case "start":
+				_, err = dockerClient.StopContainer(id)
+				if err != nil {
+					ctx.JSON(500, gin.H{
+						"result":  "error",
+						"message": fmt.Sprintf("failed to start container: %v", err),
+					})
+					return
+				}
+				ctx.JSON(200, gin.H{
+					"result":  "ok",
+					"message": fmt.Sprintf("container %s started successfully", id),
+				})
+			case "restart":
+				_, err = dockerClient.RestartContainer(id)
+				if err != nil {
+					ctx.JSON(500, gin.H{
+						"result":  "error",
+						"message": fmt.Sprintf("failed to restart container: %v", err),
+					})
+					return
+				}
+				ctx.JSON(200, gin.H{
+					"result":  "ok",
+					"message": fmt.Sprintf("container %s restarted successfully", id),
+				})
+
 			}
 		})
 	}
